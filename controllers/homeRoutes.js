15c2b47c85408acf07c573926c051c3a78ed9263
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-01-11 00:03:07 -0600

    homeRoutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,69 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+        // Get all projects and JOIN with user data
+        const postData = await Post.findAll({
+            include: [
+                { model: Comment }, { model: User }
+            ],
+        });
+
+        // Serialize data so the template can read it
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                { model: Comment }, { model: User }
+            ],
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with that id!' });
+            return;
+        }
+        const post = postData.get({ plain: true });
+
+        res.render('post', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+});
+
+module.exports = router;
