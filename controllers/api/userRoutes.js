1d5fb3941626c61e3a6d06ae3176e702fa639b80
Brianna Woodruff <briannaewoodruff@gmail.com>
2022-01-11 00:46:13 -0600

    userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,33 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 
+router.get('/', async (req, res) => {
+    try {
+        const userData = await User.findAll({
+            attributes: { exclude: ['password'] }
+        });
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const userData = await Product.findByPk(req.params.id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }, { model: Comment }],
+        });
+        if (!userData) {
+            res.status(404).json({ message: 'No user found with that id!' });
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 router.post('/', async (req, res) => {
     try {
         const userData = await User.create({
